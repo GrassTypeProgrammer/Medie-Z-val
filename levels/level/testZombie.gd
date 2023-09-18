50a696307974a9b5638f2ae8da8c15f3f2a2053a
iloste <34749898+iloste@users.noreply.github.com>
2023-09-18 20:31:06 +0100

    Zombie navigation
    Set up a navigationregion2d and navagent2d so the zombies will follow the cursor.

---
+++ levels/level/testZombie.gd
@@ -0,0 +1,13 @@
+extends NavigationAgent2D
+
+var thisRID;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	thisRID = NavigationServer2D.agent_create();
+	NavigationServer2D.agent_set_position(thisRID, Vector2(-500, -500));
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+#	NavigationServer2D.
+	pass;	
